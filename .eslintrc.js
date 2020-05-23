ff238140cd82a2214d1a5d6f937e84ba7f6addcb
hitmanet <nathan200033@gmail.com>
2020-05-23 15:54:03 +0300

    feat(linting): stylelint и eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,37 @@ module.exports = {
   extends: [
     require.resolve('@ttteam-org/frontend-configs/eslintrc')
   ],
+  overrides: [
+    {
+      files: [
+        "./src/**/*.ts"
+      ],
+      rules: {
+        "ordered-imports": "off",
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              "@/*",
+              "**/components/**/*",
+              "**/contexts/**/*",
+              "**/dashboard/**/*",
+              "**/ui/**/*",
+              "**/utils/**/*",
+              "**/widget-helpers/**/*",
+              "**/widgets/**/*"
+            ]
+          }
+        ]
+      }
+    },
+    {
+      files: [
+        "./src/**/index.stories.tsx"
+      ],
+      rules: {
+        "import/no-default-export": ["off"]
+      }
+    }
+  ],
 };
---
+++ stylelint.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // eslint-disable-next-line global-require
+  ...require('@ttteam-org/frontend-configs/stylelintrc'),
+};
