3a1dec77de06d18f3c60ea02a019c79bfd663769
hitmanet <nathan200033@gmail.com>
2020-05-25 14:54:50 +0300

    feat(app-config): добавление appConfig
    Jira issue: VEGA-100

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 
 import './App.css';
 
-export const App = (): React.ReactElement => <div className="App">{/* add your code here */}</div>;
+export const App = (): React.ReactElement => (
+  <div className="App">{/* insert your code here */}</div>
+);
