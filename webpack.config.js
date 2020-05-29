e432718d79ea4a7f3d30c60bc4da2d621f038f8e
Dmitriy Klimenko <33551076+hitmanet@users.noreply.github.com>
2020-05-29 16:10:53 +0300

    VEGA-100: Создать .env файл для переменных окружения (#3)
    * feat(github): workflow для деплоя
    
    Jira issue: VEGA-102
    
    * fix(github): фикс установки пакетов
    
    Jira issue: VEGA-102
    
    * fix(workflows): тест конфигв
    
    Jira issue: VEGA-102
    
    * fix(workflows): фикс ворфлоу
    
    Jira issue: VEGA-102
    
    * feat(github): добавление ci
    
    Jira issue: VEGA-102
    
    * fix(github): добавление токена к ci
    
    Jira issue: VEGA-102
    
    * fix(github): добавление registry-url
    
    Jira issue: VEGA-102
    
    * feat(github): добавление линков на жиру и pr_template
    
    Jira issue: VEGA-102
    
    * fix(workflow): фикс конфига
    
    Jira issue: VEGA-102
    
    * fix(workflow): изменить начертание
    
    Jira issue: VEGA-102
    
    * fix(workflow): фикс файла
    
    Jira issue: VEGA-102
    
    * feat(app-config): добавление appConfig
    
    Jira issue: VEGA-100
    
    * feat(env): добавление .env.example
    
    Jira issue: VEGA-100
    
    * chore(package): апдейт зависимостей
    
    Jira issue: VEGA-100

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
+const merge = require('webpack-merge');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-const path = require('path');
-const root = path.resolve(__dirname);
-const port = 3000;
-const entry = path.join(root, '/src/index.tsx');
-const mainWebpack = require('@ttteam-org/frontend-configs/webpack.config')({ root, port, entry, postCssConfig: require('./postcss.config') });
+const appConfig = require('./app-config')();
 
-module.exports = {
-  ...mainWebpack,
+const mainWebpack = require('@ttteam-org/frontend-configs/webpack.config')({
+  appConfig,
+  // eslint-disable-next-line global-require
+  postCssConfig: require('./postcss.config'),
+});
+
+const commonWebpack = () => {
+  const envConfig = dotenv.config();
+
+  const env = envConfig.error ? {} : envConfig.parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    // eslint-disable-next-line no-param-reassign
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    plugins: [new webpack.DefinePlugin(envKeys)],
+  };
 };
+
+module.exports = merge(commonWebpack(), mainWebpack);
