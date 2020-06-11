9dced4522e160e8ce1fba7f0a0da90669f6e04e5
Oleg Denisov <c1n1kk@gmail.com>
2020-06-11 12:39:05 +0300

    feat(auth-page): добавить форму
    Jira issue: VEGA-168

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,7 @@
+const postcssNestedPlugin = require('postcss-nested');
+
 module.exports = {
   // eslint-disable-next-line global-require
   ...require('@gpn-prototypes/frontend-configs/postcss.config'),
+  plugins: [postcssNestedPlugin].filter(Boolean),
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { presetGpnDark, Theme } from '@gpn-design/uikit/Theme';
 
 import { AuthPage } from './pages/auth';
 
 import './App.css';
 
 export const App = (): React.ReactElement => (
-  <div className="App">
+  <Theme className="App" preset={presetGpnDark}>
     <AuthPage />
-  </div>
+  </Theme>
 );
