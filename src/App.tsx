fdeba5b03f31ca8f2b0b2a63331d5153053a1bd0
Oleg Denisov <c1n1kk@gmail.com>
2020-06-24 12:41:18 +0300

    chore(global): обновить зависимости
    Jira issue: VEGA-144

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css';
 
 export const App = (): React.ReactElement => (
   <Theme className="App" preset={presetGpnDark}>
-    <AuthPage />
     <Layout>{/* insert your code here */}</Layout>
+    <AuthPage />
   </Theme>
 );
