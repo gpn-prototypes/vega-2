f9fe5c467537444aa85ea90f806b04426d3ea15a
hitmanet <nathan200033@gmail.com>
2020-07-07 15:06:09 +0300

    feat(tests): добавление утилиты для e2e
    Jira issue: VEGA-198

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
-const setupTestFile = path.resolve('setup-tests.ts')
+const setupTestFile = path.resolve('setup-tests.ts');
+
+const config = require('@gpn-prototypes/frontend-configs/jest/jest.config')({ setupFilesAfterEnv: setupTestFile });
 
 module.exports = {
-  ...require('@gpn-prototypes/frontend-configs/jest/jest.config')({ setupFilesAfterEnv: setupTestFile }),
+  ...config,
+  modulePathIgnorePatterns: [...config.modulePathIgnorePatterns, '/e2e-tests/'],
+  coveragePathIgnorePatterns: [...config.coveragePathIgnorePatterns, '/e2e-tests/'],
 };
