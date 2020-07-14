ecf2ea678048702454cb358fdb4c7c48821e144a
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2020-07-14 11:46:46 +0300

    chore(getAppConfig): почистил getAppConfig от неиспользуемых свойств
    Jira issue: VEGA-39

--- app-config/index.js
+++ app-config/index.js
@@ -3,14 +3,10 @@ const path = require('path');
 const root = process.env.PROJECT_ROOT_DIR || path.join(__dirname, '..');
 
 const getAppConfig = () => ({
-  entry: path.join(root, 'src/index.tsx'),
   root,
-  buildDirPath: path.resolve(root, 'dist'),
-  mode: process.env.NODE_ENV || 'development',
-  env: process.env.VEGA_ENV || 'development',
-  assetsPath: process.env.ASSETS_PATH || '/assets',
+  entry: path.join(root, 'src/index.tsx'),
   port: process.env.PORT || 3000,
-  apiURL: process.env.API_URL || '',
+  mode: process.env.NODE_ENV || 'development',
   analyze: process.env.ANALYZE || 0,
 });
 
