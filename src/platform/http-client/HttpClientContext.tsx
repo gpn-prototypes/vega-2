ebc91b895d25716bb5e2805906429080af3eae1b
hitmanet <nathan200033@gmail.com>
2020-07-22 18:50:53 +0300

    feat(auth): интеграция авторизации в ui
    Jira issue: VEGA-194

--- src/platform/http-client/HttpClientContext.tsx
+++ src/platform/http-client/HttpClientContext.tsx
@@ -3,8 +3,10 @@ import Cookie from 'universal-cookie';
 
 import { HTTPClient } from './http-client';
 
+const { baseApiUrl, useApiProxy, apiPath } = window.appConfig;
+
 export const HTTPClientContext = createContext<HTTPClient>(
-  new HTTPClient(new Cookie(), { baseApiUrl: '' }),
+  new HTTPClient(new Cookie(), { baseApiUrl, useApiProxy, apiPath }),
 );
 
 export const useHttpClient = (): HTTPClient => useContext(HTTPClientContext);
