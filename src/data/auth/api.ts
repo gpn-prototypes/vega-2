6b11cfcac908d2cd5965018dd877f5f9cf65c1f6
hitmanet <nathan200033@gmail.com>
2020-07-22 18:50:53 +0300

    feat(auth): интеграция авторизации в ui
    Jira issue: VEGA-194

--- src/data/auth/api.ts
+++ src/data/auth/api.ts
@@ -2,5 +2,5 @@ import { HTTPClient } from '@vega/platform/http-client';
 
 import { AuthData, AuthResponse } from './types';
 
-export const login = (client: HTTPClient, params: AuthData): Promise<AuthResponse> =>
-  client.request({ url: '/auth', method: 'get', params });
+export const login = (client: HTTPClient, data: AuthData): Promise<AuthResponse> =>
+  client.request({ url: '/auth', method: 'post', data });
