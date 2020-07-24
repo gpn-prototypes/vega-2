a169b24b144e2d727e8950ac545cc53d0d7d25c8
hitmanet <nathan200033@gmail.com>
2020-07-24 13:11:40 +0300

    fix(jest): фикс конфигов
    Jira issue: VEGA-194

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,8 @@ module.exports = {
   modulePathIgnorePatterns: [...config.modulePathIgnorePatterns, '/e2e-tests/'],
   coveragePathIgnorePatterns: [...config.coveragePathIgnorePatterns, '/e2e-tests/'],
   transformIgnorePatterns: ['node_modules/?!(@gpn-prototypes)/'],
+  moduleNameMapper: {
+    ...config.moduleNameMapper,
+    '^@vega(.*)$': '<rootDir>/src$1',
+  },
 };
