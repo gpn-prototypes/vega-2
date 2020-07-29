8e340bc9a50be0f18c3e10bd63a8babf3faac7a7
hitmanet <nathan200033@gmail.com>
2020-07-29 14:52:57 +0300

    test(e2e): добавление e2e-тестов для валидации
    Jira issue: VEGA-265

--- e2e-tests/nightwatch/tests/Регресс/1.Авторизация.test.js
+++ e2e-tests/nightwatch/tests/Регресс/1.Авторизация.test.js
@@ -40,15 +40,60 @@ testcase('Ошибки при валидации', () => {
     browser.setValue('[data-testid="AuthForm:loginInput"] > input', 'test@m..');
   });
 
+  step('вводим корректный пароль', () => {
+    browser.setValue('[data-testid="AuthForm:passwordInput"] > input', 'password');
+  });
+
   step('нажимаем на кнопку Войти', () => {
     browser.click('[data-testid="AuthForm:submit"]');
   });
 
   expected('отобразилась ошибка для логина', () => {
-    browser.waitForElementPresent('.TextField_state_alert', 2000);
+    browser.waitForElementPresent('[data-testid="AuthForm:loginInput"].TextField_state_alert', 2000);
     browser.assert.screenshotElement(
       '[data-test-id="AuthPage:form"]',
       'ошибка авторизации при некорректном email'
+    );
+  });
+
+  step('обновляем браузер', () => {
+    browser.refresh();
+  });
+
+  step('нажимаем на кнопку Войти', () => {
+    browser.click('[data-testid="AuthForm:submit"]');
+  });
+
+  expected('отобразилась ошибка о том, что данные отсутствуют', () => {
+    browser.waitForElementPresent('[data-testid="AuthForm:loginInput"].TextField_state_alert', 2000);
+    browser.waitForElementPresent('[data-testid="AuthForm:passwordInput"].TextField_state_alert', 2000);
+    browser.assert.screenshotElement(
+      '[data-test-id="AuthPage:form"]',
+      'ошибка авторизации при отсутствии данных'
+    );
+  });
+
+  step('обновляем браузер', () => {
+    browser.refresh();
+  });
+
+  step('вводим корректный логин', () => {
+    browser.setValue('[data-testid="AuthForm:loginInput"] > input', admin.login);
+  });
+
+  step('вводим некорректный пароль', () => {
+    browser.setValue('[data-testid="AuthForm:passwordInput"] > input', '{{{[[]]}}}');
+  });
+
+  step('нажимаем на кнопку Войти', () => {
+    browser.click('[data-testid="AuthForm:submit"]');
+  });
+
+  expected('отобразилась ошибка для пароля', () => {
+    browser.waitForElementPresent('[data-testid="AuthForm:passwordInput"].TextField_state_alert', 2000);
+    browser.assert.screenshotElement(
+      '[data-test-id="AuthPage:form"]',
+      'ошибка авторизации при некорректном пароле'
     );
   });
 });
