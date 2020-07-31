632203844e4163bb2320eaf0f64e9eed23fb3cae
hitmanet <nathan200033@gmail.com>
2020-07-31 16:26:59 +0300

    refactor(validate): отрефакторил валидацию
    Jira issue: VEGA-169

--- src/platform/validation/multiple-validation.ts
+++
@@ -1,6 +0,0 @@
-import { compose, find, is, juxt } from 'ramda';
-
-import { MultipleValidation, ValidatorFn } from './types';
-
-export const multipleValidation = (validators: ValidatorFn[]): MultipleValidation =>
-  compose(find(is(String)), juxt(validators));
--- src/platform/validation/types.ts
+++
@@ -1,5 +0,0 @@
-export type ValidatorFn = (value: string) => string | undefined;
-export type MultipleValidation = (x: string) => ValidatorFn;
-export type ValidationScheme = {
-  [key: string]: ValidatorFn | MultipleValidation;
-};
