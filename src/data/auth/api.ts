377d055c02b52c6ff19baf524a9476d362fea92b
hitmanet <nathan200033@gmail.com>
2020-08-13 11:13:22 +0300

    fix(auth): исправлен урл на авторизацию
    Jira issue: VEGA-194

--- src/data/auth/api.ts
+++ src/data/auth/api.ts
@@ -2,8 +2,8 @@ import { APIClient } from '@vega/platform/api-client';
 
 import { AuthResponse, Credentials } from './types';
 
-export const login = (client: APIClient, params: Credentials): Promise<AuthResponse> =>
-  client.request({ url: '/krb', method: 'post', params });
+export const login = (client: APIClient, data: Credentials): Promise<AuthResponse> =>
+  client.request({ url: '/auth', method: 'post', data });
 
 export const getCurrentUser = (client: APIClient): Promise<void> =>
   client.request({ url: '/krb-user', method: 'get', withAuth: true });
