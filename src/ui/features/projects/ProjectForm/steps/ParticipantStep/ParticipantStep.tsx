0b09d0330d1602f6ecef5dcf1dc8fc79780f5f7d
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-10-08 14:27:54 +0300

    feat(participant-step): добавить верстку для таблицы
    Jira issue: VEGA-408

--- src/ui/features/projects/ProjectForm/steps/ParticipantStep/ParticipantStep.tsx
+++ src/ui/features/projects/ProjectForm/steps/ParticipantStep/ParticipantStep.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import { Button, Table, useModal } from '@gpn-prototypes/vega-ui';
+import { Avatar, Button, Table, Text, useModal } from '@gpn-prototypes/vega-ui';
 
 import { cnProjectForm } from '../../cn-form';
 
+import { cnParticipantStep } from './cn-participant-step';
 import { ParticipantForm } from './ParticipantForm';
 
+import './ParticipantStep.css';
+
 type StepProps = Record<string, unknown>;
+
+const cn = {
+  root: cnParticipantStep().toString(),
+  table: cnParticipantStep('table').toString(),
+  footer: cnParticipantStep('footer').toString(),
+  rowName: cnParticipantStep('rowName').toString(),
+  rowAvatar: cnParticipantStep('rowAvatar').toString(),
+};
 
 export const ParticipantStep: React.FC<StepProps> = () => {
   const columns = [
     { title: 'Имя', accessor: 'name' },
-    { title: 'Роль', accessor: 'role' },
+    { title: 'Роль', accessor: 'role', width: 252 },
   ];
 
   const { isOpen, close, open } = useModal();
 
-  const rows = [{ id: '1', name: 'Иван', role: 'Админ' }];
+  const rows = [
+    {
+      id: '1',
+      name: (
+        <div className={cn.rowName}>
+          <div className={cn.rowAvatar}>
+            <Avatar size="s" name="Иван Иванов И." />
+          </div>
+          <Text size="s">Иван Иванов И.</Text>
+        </div>
+      ),
+      role: <Text size="s">Геолог</Text>,
+    },
+  ];
 
   const handleSubmit = (values: unknown) => {
     console.log(values);
@@ -23,11 +47,13 @@ export const ParticipantStep: React.FC<StepProps> = () => {
 
   return (
     <div className={cnProjectForm('Step')}>
-      <div>
-        <Button type="button" label="+ Добавить участника" view="ghost" onClick={open} />
-        <ParticipantForm isOpen={isOpen} onClose={close} onSubmit={handleSubmit} />
+      <div className={cn.root}>
+        <Table columns={columns} rows={rows} className={cn.table} verticalAlign="center" />
+        <div className={cn.footer}>
+          <Button type="button" label="+ Добавить участника" view="ghost" onClick={open} />
+          <ParticipantForm isOpen={isOpen} onClose={close} onSubmit={handleSubmit} />
+        </div>
       </div>
-      <Table columns={columns} rows={rows} />
     </div>
   );
 };
