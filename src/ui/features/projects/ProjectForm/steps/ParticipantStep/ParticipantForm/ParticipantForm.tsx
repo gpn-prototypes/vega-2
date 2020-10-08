6bfb048c893516ac08d1f8a56616a8e1acd4deb9
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-10-08 16:54:54 +0300

    feat(participant-form): доделать верстку. Добавить комбобокс
    Jira issue: VEGA-408

--- src/ui/features/projects/ProjectForm/steps/ParticipantStep/ParticipantForm/ParticipantForm.tsx
+++ src/ui/features/projects/ProjectForm/steps/ParticipantStep/ParticipantForm/ParticipantForm.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Field, Form } from 'react-final-form';
-import { Button, Form as VegaForm, Modal, Text, TextField } from '@gpn-prototypes/vega-ui';
+import { Combobox } from '@consta/uikit/Combobox';
+import { MultiCombobox } from '@consta/uikit/MultiCombobox';
+import { Button, Form as VegaForm, Modal, Text } from '@gpn-prototypes/vega-ui';
+
+import { cnParticipantForm } from './cn-participant-form';
+
+import './ParticipantForm.css';
 
 type FormValues = {
   user: string;
@@ -13,6 +19,31 @@ type ParticipantProps = {
   onSubmit: (values: FormValues) => void;
 };
 
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+const cn = {
+  root: cnParticipantForm().toString(),
+  footer: cnParticipantForm('footer').toString(),
+  footerItem: cnParticipantForm('footerItem').toString(),
+};
+
+const roles = [
+  { label: 'Геолог', value: '1' },
+  { label: 'Менеджер', value: '2' },
+  { label: 'Геолог', value: '3' },
+  { label: 'Менеджер', value: '4' },
+];
+
+const users = [
+  { label: 'Иванов Иван', value: '1' },
+  { label: 'Васильев Василий', value: '2' },
+];
+
+const getItemLabel = (option: SelectOption): string => option.label;
+
 export const ParticipantForm: React.FC<ParticipantProps> = (props) => {
   const { onClose, isOpen } = props;
 
@@ -21,7 +52,7 @@ export const ParticipantForm: React.FC<ParticipantProps> = (props) => {
   };
 
   return (
-    <Modal hasOverlay hasCloseButton onClose={onClose} isOpen={isOpen}>
+    <Modal hasOverlay hasCloseButton onClose={onClose} isOpen={isOpen} className={cn.root}>
       <Modal.Header>
         <Text size="xs">Добавить участника</Text>
       </Modal.Header>
@@ -41,16 +72,14 @@ export const ParticipantForm: React.FC<ParticipantProps> = (props) => {
                       <Field
                         name="user"
                         render={({ input }): React.ReactNode => (
-                          <TextField
-                            id="name"
-                            size="s"
-                            width="full"
+                          <Combobox
+                            id="user"
+                            options={users}
+                            getOptionLabel={getItemLabel}
                             placeholder="Пользователь"
-                            name={input.name}
-                            value={input.value}
-                            onChange={({ e }): void => input.onChange(e)}
-                            onBlur={input.onBlur}
-                            onFocus={input.onFocus}
+                            onChange={(value: SelectOption | null): void => {
+                              input.onChange(value);
+                            }}
                           />
                         )}
                       />
@@ -64,16 +93,14 @@ export const ParticipantForm: React.FC<ParticipantProps> = (props) => {
                       <Field
                         name="roles"
                         render={({ input }): React.ReactNode => (
-                          <TextField
+                          <MultiCombobox
                             id="roles"
-                            size="s"
-                            width="full"
+                            options={roles}
+                            getOptionLabel={getItemLabel}
                             placeholder="Доступ"
-                            name={input.name}
-                            value={input.value}
-                            onChange={({ e }): void => input.onChange(e)}
-                            onBlur={input.onBlur}
-                            onFocus={input.onFocus}
+                            onChange={(value: SelectOption[] | null): void => {
+                              input.onChange(value);
+                            }}
                           />
                         )}
                       />
@@ -82,14 +109,21 @@ export const ParticipantForm: React.FC<ParticipantProps> = (props) => {
                 </VegaForm>
               </Modal.Body>
 
-              <Modal.Footer>
+              <Modal.Footer className={cn.footer}>
                 <Button
                   size="m"
                   view="primary"
                   label="Пригласить в проект"
                   onClick={handleSubmit}
+                  className={cn.footerItem}
                 />
-                <Button size="m" view="ghost" label="Отмена" onClick={onClose} />
+                <Button
+                  size="m"
+                  view="ghost"
+                  label="Отмена"
+                  onClick={onClose}
+                  className={cn.footerItem}
+                />
               </Modal.Footer>
             </>
           );
