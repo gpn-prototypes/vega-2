8ee46d2dbb6bc577bfcffbd55d6028dc48239e06
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-10-21 07:30:36 +0300

    refactor(babel): убрать коммент
    Jira issue: VEGA-408

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 const base = require('@gpn-prototypes/frontend-configs/babel.config');
 
 module.exports = {
-  // eslint-disable-next-line global-require
   ...base,
   plugins: [...base.plugins],
 };
