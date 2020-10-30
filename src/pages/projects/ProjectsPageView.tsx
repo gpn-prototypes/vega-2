3d55ddd1cbb9ffaaf8317ddd71b22ed0198b4cd7
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-10-30 08:12:44 +0300

    fix(projects): исправить ошибки после обновления пакетов

--- src/pages/projects/ProjectsPageView.tsx
+++ src/pages/projects/ProjectsPageView.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { IconProps } from '@gpn-design/uikit/Icon';
+import { IconProps } from '@consta/uikit/Icon';
 import {
   Button,
   ChoiceGroup,
