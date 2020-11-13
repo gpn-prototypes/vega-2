8ef7643ff90ed5336679681ed6703d1d30555f6c
Oleg <c1n1kk@gmail.com>
2020-11-13 19:01:12 +0300

    VEGA-630: Прокинуть переменные окружения в сборку (#59)
    * fix(env): исправить добавление переменных окружения
    
    * fix(env): добавить переменные окружения

--- webpack.forShellConfig.js
+++ webpack.forShellConfig.js
@@ -1,10 +1,19 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const singleSpaDefaults = require('webpack-config-single-spa-react-ts');
 const ImportMapPlugin = require('webpack-import-map-plugin');
 const { getAppConfig } = require('./app-config');
 
-const { projectName, port } = getAppConfig();
+const { projectName, port: appPort } = getAppConfig();
+
+function getPort(webpackConfigEnv) {
+  let port = process.env.PORT || 3000;
+  if (webpackConfigEnv !== undefined && 'port' in webpackConfigEnv) {
+    port = webpackConfigEnv.port;
+  }
+  return port;
+}
 
 module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
@@ -12,6 +21,20 @@ module.exports = (webpackConfigEnv) => {
     projectName,
     webpackConfigEnv,
   });
+
+  const PORT = getPort(webpackConfigEnv);
+  const YC_DEPLOYMENT = process.env.YC_DEPLOYMENT === 'true'; // Yandex Cloud Deployment
+  const NODE_ENV = process.env.NODE_ENV || 'development';
+  const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+  const envConfig = dotenv.config();
+
+  const env = envConfig.error ? {} : envConfig.parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    // eslint-disable-next-line no-param-reassign
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
 
   const config = webpackMerge.smart(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
@@ -38,9 +61,16 @@ module.exports = (webpackConfigEnv) => {
       ],
     },
     plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+        'process.env.YC_DEPLOYMENT': JSON.stringify(YC_DEPLOYMENT),
+        'process.env.BASE_API_URL': JSON.stringify(process.env.BASE_API_URL),
+        'process.env.BASE_URL': JSON.stringify(BASE_URL),
+        ...envKeys,
+      }),
       new ImportMapPlugin({
         fileName: 'import-map.json',
-        baseUrl: process.env.BASE_URL || `http://locahost:${port}`,
+        baseUrl: process.env.BASE_URL || `http://locahost:${appPort}`,
         filter(x) {
           return ['main.js'].includes(x.name);
         },
