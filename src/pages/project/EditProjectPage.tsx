47dc01bba21346f5e3c42cce4e67542146a31e19
Gurinovich Roman <31928264+fixmylie@users.noreply.github.com>
2020-11-16 17:54:02 +0300

    VEGA-642: Добавить негативные уведомления на страницы (#70)
    * feat(project): вывод ошибок в редактировании
    
    Jira issue: VEGA-642
    
    * fix(snackbar): исправить позиционирование
    
    Jira issue: VEGA-642

--- src/pages/project/EditProjectPage.tsx
+++ src/pages/project/EditProjectPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Loader } from '@gpn-prototypes/vega-ui';
 
 import { Project, ProjectStatusEnum } from '../../__generated__/types';
+import { useSnackbar } from '../../providers/snackbar';
 import { FormValues, ProjectForm } from '../../ui/features/projects';
 
 import { useQueryProject, useQueryRegionList, useUpdateProject2 } from './__generated__/project';
@@ -37,6 +38,7 @@ const getInitialValues = (project: ProjectType): FormValues => {
 
 export const EditProjectPage: React.FC<PageProps> = () => {
   const { projectId } = useParams<ParamsType>();
+  const snackbar = useSnackbar();
 
   const {
     data: queryProjectData,
@@ -58,14 +60,11 @@ export const EditProjectPage: React.FC<PageProps> = () => {
 
   const referenceData: ReferenceDataType = { regionList: queryRegionListData?.regionList };
 
-  const handleFormSubmit = (values: FormValues) => {
-    // eslint-disable-next-line no-console
-    console.log('handleFormSubmit', values);
-
+  const handleFormSubmit = async (values: FormValues) => {
     const version =
       queryProjectData?.project?.__typename === 'Project' ? queryProjectData?.project?.version : 1;
 
-    updateProject({
+    const updateProjectResult = await updateProject({
       variables: {
         vid: projectId,
         name: values.description.name,
@@ -81,6 +80,16 @@ export const EditProjectPage: React.FC<PageProps> = () => {
         version: version || 1,
       },
     });
+
+    if (updateProjectResult.data?.updateProject?.result?.__typename === 'Error') {
+      const inlineUpdateProjectError = updateProjectResult.data?.updateProject?.result;
+
+      snackbar.addItem({
+        key: `${inlineUpdateProjectError.code}-update-error`,
+        status: 'alert',
+        message: inlineUpdateProjectError.message,
+      });
+    }
   };
 
   if (queryProjectLoading || queryRegionListLoading) {
