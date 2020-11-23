a26b08ba6f1d93f1343baa19f29cc13787197c5a
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2020-11-23 10:29:00 +0300

    feat(global): добавил механизм отмены создания проекта
    Jira issue: VEGA-586

--- src/pages/project/CreateProjectPage.tsx
+++ src/pages/project/CreateProjectPage.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Loader } from '@gpn-prototypes/vega-ui';
+import { Button, Loader, Modal, Text } from '@gpn-prototypes/vega-ui';
 
 import { ProjectStatusEnum } from '../../__generated__/types';
 import { useSnackbar } from '../../providers/snackbar';
 import { FormValues, ProjectForm } from '../../ui/features/projects';
 
-import { useCreateProject, useQueryRegionList, useUpdateProject2 } from './__generated__/project';
+import {
+  useCreateProject,
+  useDeleteProject2,
+  useQueryRegionList,
+  useUpdateProject2,
+} from './__generated__/project';
 import { cnPage } from './cn-page';
 import { ReferenceDataType } from './types';
 
@@ -20,11 +25,16 @@ export const CreateProjectPage: React.FC<PageProps> = () => {
   const history = useHistory();
   const snackbar = useSnackbar();
 
+  const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
+
   const [blankProjectId, setBlankProjectId] = useState<string | undefined>(undefined);
 
   const [createProject, { error: createProjectError }] = useCreateProject();
 
   const [updateProject, { error: updateProjectError }] = useUpdateProject2();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [deleteProject, { error: deleteProjectError }] = useDeleteProject2();
 
   useEffect(() => {
     let isCancelled = false;
@@ -107,6 +117,20 @@ export const CreateProjectPage: React.FC<PageProps> = () => {
     }
   };
 
+  const handleCancel = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleAbort = async () => {
+    await deleteProject({ variables: { vid: blankProjectId } });
+
+    localStorage.removeItem(BLANK_PROJECT_ID);
+
+    setIsModalOpen(false);
+
+    history.push('/projects');
+  };
+
   if (createProjectError || updateProjectError || queryRegionListError) {
     // eslint-disable-next-line no-console
     console.log({ createProjectError, updateProjectError, queryRegionListError });
@@ -120,7 +144,40 @@ export const CreateProjectPage: React.FC<PageProps> = () => {
 
   return (
     <div className={cnPage()}>
-      <ProjectForm mode="create" referenceData={referenceData} onSubmit={handleFormSubmit} />
+      <ProjectForm
+        mode="create"
+        referenceData={referenceData}
+        onSubmit={handleFormSubmit}
+        onCancel={handleCancel}
+      />
+      <Modal
+        hasOverlay
+        hasCloseButton
+        isOpen={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+        }}
+        className={cnPage('Modal').toString()}
+      >
+        <Modal.Header>
+          <Text size="xs">Создание проекта</Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Text>Вы уверены, что хотите прервать создание проекта?</Text>
+        </Modal.Body>
+        <Modal.Footer className={cnPage('ModalFooter').toString()}>
+          <Button
+            size="m"
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+            view="primary"
+            label="Нет, продолжить заполнение"
+            className={cnPage('ButtonCancel').toString()}
+          />
+          <Button size="m" onClick={handleAbort} view="ghost" label="Да, прервать" />
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
