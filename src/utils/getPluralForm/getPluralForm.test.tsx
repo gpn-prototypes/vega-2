1f99e6d375d30ad45b67922edd63e7b0c8f0ab8f
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2021-01-12 11:58:49 +0300

    feat(getPluralForm): доработал утилиту getPluralForm (#125)
    Jira issue: VEGA-368

--- src/utils/getPluralForm/getPluralForm.test.tsx
+++ src/utils/getPluralForm/getPluralForm.test.tsx
@@ -1,35 +1,95 @@
 import { getPluralForm } from './index';
 
 describe('getPluralForm', () => {
-  test('первая форма', () => {
-    expect(getPluralForm(1, ['форма', 'формы', 'форм'])).toBe('форма');
+  describe('параматры по умолчанию', () => {
+    const pluralForm = getPluralForm(['минута', 'минуты', 'минут']);
+
+    test('первая форма', () => {
+      expect(pluralForm(1)).toBe('минута');
+    });
+
+    test('вторая форма', () => {
+      expect(pluralForm(2)).toBe('минуты');
+    });
+
+    test('третья форма', () => {
+      expect(pluralForm(5)).toBe('минут');
+    });
+
+    test('дробное значение, меньше 5', () => {
+      expect(pluralForm(2.23)).toBe('минуты');
+    });
+
+    test('дробное значение, больше 5', () => {
+      expect(pluralForm(7.77)).toBe('минут');
+    });
+
+    test('ноль', () => {
+      expect(pluralForm(0)).toBe('минут');
+    });
+
+    test('отрицательное значение', () => {
+      expect(pluralForm(-2233)).toBe('минуты');
+    });
+
+    test('числовое значение не передано', () => {
+      expect(pluralForm(null)).toBe('');
+    });
   });
 
-  test('вторая форма', () => {
-    expect(getPluralForm(2, ['форма', 'формы', 'форм'])).toBe('формы');
+  describe('параметры includeNumber и delimiter', () => {
+    test('includeNumber = false и delimiter = undefined', () => {
+      const pluralForm = getPluralForm(['минута', 'минуты', 'минут'], {
+        includeNumber: false,
+        delimiter: undefined,
+      });
+      expect(pluralForm(1)).toBe('минута');
+    });
+
+    test('includeNumber = false и delimiter = "-"', () => {
+      const pluralForm = getPluralForm(['минута', 'минуты', 'минут'], {
+        includeNumber: false,
+        delimiter: '-',
+      });
+      expect(pluralForm(1)).toBe('минута');
+    });
+
+    test('includeNumber = true и delimiter = undefined', () => {
+      const pluralForm = getPluralForm(['минута', 'минуты', 'минут'], {
+        includeNumber: true,
+        delimiter: undefined,
+      });
+      expect(pluralForm(1)).toBe('1 минута');
+    });
+
+    test('includeNumber = true и delimiter = "-"', () => {
+      const pluralForm = getPluralForm(['минута', 'минуты', 'минут'], {
+        includeNumber: true,
+        delimiter: '-',
+      });
+      expect(pluralForm(1)).toBe('1-минута');
+    });
   });
 
-  test('третья форма', () => {
-    expect(getPluralForm(5, ['форма', 'формы', 'форм'])).toBe('форм');
-  });
+  describe('параметр discardFractionalPart', () => {
+    test('discardFractionalPart = true', () => {
+      const pluralForm = getPluralForm(['минута', 'минуты', 'минут'], {
+        includeNumber: false,
+        delimiter: undefined,
+        discardFractionalPart: true,
+      });
 
-  test('числовое значение не передано', () => {
-    expect(getPluralForm(null, ['форма', 'формы', 'форм'])).toBe('');
-  });
+      expect(pluralForm(1.55)).toBe('минута');
+    });
 
-  test('дробное значение, меньше 5', () => {
-    expect(getPluralForm(2.23, ['форма', 'формы', 'форм'])).toBe('формы');
-  });
+    test('discardFractionalPart = false', () => {
+      const pluralForm = getPluralForm(['минута', 'минуты', 'минут'], {
+        includeNumber: true,
+        delimiter: undefined,
+        discardFractionalPart: false,
+      });
 
-  test('дробное значение, больше 5', () => {
-    expect(getPluralForm(7.77, ['форма', 'формы', 'форм'])).toBe('форм');
-  });
-
-  test('отрицательное значение', () => {
-    expect(getPluralForm(-2233, ['форма', 'формы', 'форм'])).toBe('формы');
-  });
-
-  test('ноль', () => {
-    expect(getPluralForm(0, ['форма', 'формы', 'форм'])).toBe('форм');
+      expect(pluralForm(1.55)).toBe('1.55 минуты');
+    });
   });
 });
--- src/utils/getPluralForm/index.ts
+++ src/utils/getPluralForm/index.ts
@@ -1,19 +1,63 @@
 /*
-  forms передавать в следующем формате: [одна единица, две единицы, пять единиц]
-  одна форма, две формы, пять форм -> ['форма', 'формы', 'форм']
+  Значения в forms необходимо передавать в следующем формате: [одна единица, две единицы, пять единиц]
+  Например, получим все формы для слова "минута": одна минута, две минуты, пять минут. В forms запишем: ['минута', 'минуты', 'минут']
 */
 
-export const getPluralForm = (number: number | null, forms: string[]): string => {
+type FormsType = [string, string, string];
+
+type OptionsType = {
+  includeNumber?: boolean;
+  delimiter?: string;
+  discardFractionalPart?: boolean;
+};
+
+/*
+  includeNumber - добавить в вывод число, например "1 минута"
+  delimiter - если включена опция includeNumber, то в этом параметре можно указать разделитель. Например, если добавить "-", то получим вывод "1-минута"
+  discardFractionalPart - отбросить дробную часть числа при получении формы. Например, с включенном опцией для 1.55 мы получим "минута", а с выключенной получим "минуты" - читаем как "одна целая и пятьдесят пять сотых минуты"
+*/
+
+const defaultOptions = {
+  includeNumber: false,
+  delimiter: ' ',
+  discardFractionalPart: true,
+};
+
+const formatResult = (
+  number: number,
+  form: string,
+  includeNumber: boolean,
+  delimiter: string,
+): string => {
+  return includeNumber ? `${number}${delimiter}${form}` : form;
+};
+
+export const getPluralForm = (forms: FormsType, options: OptionsType = defaultOptions) => (
+  number: number | null,
+): string => {
   if (number === null) {
     return '';
+  }
+
+  const {
+    includeNumber = defaultOptions.includeNumber,
+    delimiter = defaultOptions.delimiter,
+    discardFractionalPart = defaultOptions.discardFractionalPart,
+  } = options;
+
+  if (!Number.isInteger(number) && !discardFractionalPart) {
+    const form = forms[1];
+    return formatResult(number, form, includeNumber, delimiter);
   }
 
   const naturalNumber = Math.trunc(Math.abs(number));
   const cases = [2, 0, 1, 1, 1, 2];
 
-  return forms[
-    naturalNumber % 100 > 4 && naturalNumber % 100 < 20
-      ? 2
-      : cases[naturalNumber % 10 < 5 ? naturalNumber % 10 : 5]
-  ];
+  const form =
+    forms[
+      naturalNumber % 100 > 4 && naturalNumber % 100 < 20
+        ? 2
+        : cases[naturalNumber % 10 < 5 ? naturalNumber % 10 : 5]
+    ];
+  return formatResult(number, form, includeNumber, delimiter);
 };
