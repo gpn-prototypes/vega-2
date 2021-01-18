4c4be76d56c7231621803872baa5bf4eb593a875
Roman Gurinovich <gurinovich.rv@gmail.com>
2021-01-18 17:34:10 +0700

    test(DescriptionStep): add test describes
    Jira issue: VEGA-842

---
+++ src/ui/features/projects/ProjectForm/steps/DescriptionStep/DescriptionStep.test.tsx
@@ -0,0 +1,17 @@
+describe('DescriptionStep', function () {
+  describe('getYearStartOptions', function () {
+    it('возвращает список лет на 10 лет вперед с текущего года', function () {});
+  });
+
+  describe('isValidYear', function () {
+    it('проверяет строку на содержание 4х значного числа', function () {});
+  });
+
+  it('рендерится без ошибок', function () {});
+
+  describe('год начала планирования', function () {
+    it('выставить значение по умолчанию, если форма создания проекта', function () {});
+    it('добавляет год в начало списка, если его нет в списке', function () {});
+    it('показывает ошибку валидации', function () {});
+  });
+});
