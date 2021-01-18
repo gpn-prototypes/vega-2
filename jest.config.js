7c642bd0ecbd42e0202a961a93ae39f2cd42cb93
Sergey Gavshin <inzephirum@gmail.com>
2021-01-18 13:20:39 +0700

    VEGA-804: Настроить test coverage для vega-sp (#133)
    * chore(jest): исключить генерируемые файлы из отчета покрытия
    
    Jira issue: VEGA-804
    
    * chore(scripts): добавить скрипт покрытия для измененных файлов
    
    Jira issue: VEGA-804
    
    * chore(ci): добавить coverage workflow
    
    Jira issue: VEGA-804

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,12 @@ const config = require('@gpn-prototypes/frontend-configs/jest/jest.config')({
 module.exports = {
   ...config,
   modulePathIgnorePatterns: [...config.modulePathIgnorePatterns, '/e2e-tests/'],
-  coveragePathIgnorePatterns: [...config.coveragePathIgnorePatterns, '/e2e-tests/'],
+  coveragePathIgnorePatterns: [
+    ...config.coveragePathIgnorePatterns,
+    '/e2e-tests/',
+    '/__generated__/',
+  ],
+  coverageReporters: ['lcov', 'json-summary', 'text', 'text-summary'],
   transformIgnorePatterns: ['node_modules/?!(@gpn-prototypes)/'],
   moduleNameMapper: {
     ...config.moduleNameMapper,
