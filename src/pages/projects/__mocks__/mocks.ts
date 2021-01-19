22c8fc3bc452cb8a8fdfbd36795070df9c4ddd42
Oleg Denisov <c1n1kk@gmail.com>
2021-01-19 16:54:09 +0300

    refactor(mocks): добавить моки

---
+++ src/pages/projects/__mocks__/mocks.ts
@@ -0,0 +1,9 @@
+import { defaultMock } from './mock-default';
+import { deleteProjectMock } from './mock-delete-project';
+import { favoriteProjectMock } from './mock-favorite-project';
+
+export const mocks = {
+  default: defaultMock,
+  deleteProject: deleteProjectMock,
+  favoriteProject: favoriteProjectMock,
+};
