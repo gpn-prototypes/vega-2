857e7103aa2d5d9e7c4d3c50de7abd598c91b129
Oleg Denisov <c1n1kk@gmail.com>
2021-01-22 16:00:49 +0300

    refactor(utils): переименовать константу

--- src/utils/check-clickable-element-in-bubble/check-clickable-element-in-bubble.ts
+++ src/utils/check-clickable-element-in-bubble/check-clickable-element-in-bubble.ts
@@ -5,12 +5,12 @@ export const checkClickableElementInBubble = (
   if (!e) {
     return hasClickable;
   }
-  const tagsForIngore = ['A', 'INPUT', 'SELECT', 'TEXTAREA', 'BUTTON'];
+  const interactiveTags = ['A', 'INPUT', 'SELECT', 'TEXTAREA', 'BUTTON'];
   const elements = e.nativeEvent.composedPath();
 
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < elements.length; i++) {
-    hasClickable = tagsForIngore.some((tag) => tag === (elements[i] as Element).tagName);
+    hasClickable = interactiveTags.some((tag) => tag === (elements[i] as Element).tagName);
     if (hasClickable) {
       break;
     }
