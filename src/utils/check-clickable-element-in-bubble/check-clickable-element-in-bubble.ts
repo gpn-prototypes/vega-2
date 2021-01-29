c1ba0cb986aa68a29655f2216f59bdc71a876bdc
Oleg Denisov <c1n1kk@gmail.com>
2021-01-29 12:07:46 +0300

    refactor(utils): изменить проверку

--- src/utils/check-clickable-element-in-bubble/check-clickable-element-in-bubble.ts
+++ src/utils/check-clickable-element-in-bubble/check-clickable-element-in-bubble.ts
@@ -8,13 +8,9 @@ export const checkClickableElementInBubble = (
   const interactiveTags = ['A', 'INPUT', 'SELECT', 'TEXTAREA', 'BUTTON'];
   const elements = e.nativeEvent.composedPath();
 
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < elements.length; i++) {
-    hasClickable = interactiveTags.some((tag) => tag === (elements[i] as Element).tagName);
-    if (hasClickable) {
-      break;
-    }
-  }
+  hasClickable = elements.some((el) =>
+    interactiveTags.some((tag) => tag === (el as Element).tagName),
+  );
 
   return hasClickable;
 };
