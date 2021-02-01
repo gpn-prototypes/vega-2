e24475f5299a652cc2776fd8f942cb7fa0b2bca2
Roman Gurinovich <gurinovich.rv@gmail.com>
2021-02-01 19:23:59 +0700

    test(CreateProjectPage): add tests
    Jira issue: VEGA-815

--- src/pages/project/__mocks__/index.ts
+++
@@ -1,3 +0,0 @@
-export { MockCreateBlankProject } from './mock-create-blank-project';
-export { MockProjectFormRegionList } from './mock-project-form-region-list';
-export { MockProjectFormFields } from './mock-project-form-fields';
