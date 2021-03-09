b5d201a353467145368451e3885756da09b07ac8
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2021-03-09 15:14:43 +0300

    VEGA-3192: [FE] Аудит зависимостей (#167)
    Jira issue: VEGA-914

--- src/providers/notifications/notificationsMock.tsx
+++ src/providers/notifications/notificationsMock.tsx
@@ -1,16 +1,20 @@
 import { Notifications } from '../../../types/notifications';
 
 export const notificationsMock: Notifications = {
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   add(props) {
     return 'string';
   },
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   remove(key) {},
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   subscribe(topic, payload) {
     return () => {};
   },
   getAll() {
     return [];
   },
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   on(action, payload) {
     return () => {};
   },
